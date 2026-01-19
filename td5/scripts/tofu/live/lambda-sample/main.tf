776b3ec484f8bd5270bd4458388e1c51aa4a656b
Mateo Gallina <mateocestmoi@MacBook-Air-de-Mateo.local>
2026-01-19 22:56:45 +0100

    Fix: module source paths

--- td5/scripts/tofu/live/lambda-sample/main.tf
+++ td5/scripts/tofu/live/lambda-sample/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "function" {
-  source = "github.com/brikis98/devops-book//ch3/tofu/modules/lambda"
+  source = source = "github.com/brikis98/devops-book//modules/lambda?ref=main"
 
   name = var.name
 
@@ -20,7 +20,7 @@ module "function" {
 }
 
 module "gateway" {
-  source = "github.com/brikis98/devops-book//ch3/tofu/modules/api-gateway"
+  source = "github.com/brikis98/devops-book//modules/api-gateway?ref=main"
 
   name = var.name
   function_arn       = module.function.function_arn
