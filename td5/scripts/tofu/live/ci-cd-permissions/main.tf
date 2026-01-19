2a0e60b4a99305116fa9b6a7ff3ec664b5b227e9
Mateo Gallina <mateocestmoi@MacBook-Air-de-Mateo.local>
2026-01-19 22:08:29 +0100

    Fix: move workflow to correct location and update IAM roles

--- td5/scripts/tofu/live/ci-cd-permissions/main.tf
+++ td5/scripts/tofu/live/ci-cd-permissions/main.tf
@@ -3,28 +3,26 @@ provider "aws" {
 }
 
 module "oidc_provider" {
-  source = "github.com/brikis98/devops-book//ch5/tofu/modules/github-aws-oidc"
+  # Chemin local vers le dossier modules/github-aws-oidc
+  source = "../../modules/github-aws-oidc"
 
   provider_url = "https://token.actions.githubusercontent.com" 
-
 }
 
 module "iam_roles" {
-  source = "github.com/brikis98/devops-book//ch5/tofu/modules/gh-actions-iam-roles"
+  # Chemin local vers le dossier modules/gh-actions-iam-roles
+  source = "../../modules/gh-actions-iam-roles"
 
-  name              = "lambda-sample"                           
-  oidc_provider_arn = module.oidc_provider.oidc_provider_arn    
+  name              = "lambda-sample"
+  oidc_provider_arn = module.oidc_provider.oidc_provider_arn
+  
+  enable_iam_role_for_testing = true
+  enable_iam_role_for_plan    = true
+  enable_iam_role_for_apply   = true
 
-  enable_iam_role_for_testing = true                            
-
-  # TODO: fill in your own repo name here!
-  github_repo      = "brikis98/fundamentals-of-devops-examples" 
-  lambda_base_name = "lambda-sample"                            
-
-  enable_iam_role_for_plan  = true                                
-  enable_iam_role_for_apply = true                                
-
-  # TODO: fill in your own bucket and table name here!
-  tofu_state_bucket         = "fundamentals-of-devops-tofu-state" 
-  tofu_state_dynamodb_table = "fundamentals-of-devops-tofu-state" 
+  # C'EST ICI QUE TU GARDES TES INFOS PERSO
+  github_repo               = "MG-esiee/mon-tp-eks"
+  lambda_base_name          = "lambda-sample"
+  tofu_state_bucket         = "fundamentals-of-devops-tofu-state"
+  tofu_state_dynamodb_table = "fundamentals-of-devops-tofu-state"
 }
